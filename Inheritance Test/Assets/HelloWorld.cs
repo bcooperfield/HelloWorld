412523e9194b2ab6102936c62ae4831a63fb7c57
Edward Ikeguchi <eikeguchi@mac.com>
2018-04-09 13:22:55 -0400

    This is a test.

---
+++ Inheritance Test/Assets/HelloWorld.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour{
+
+public string anyString
+{
+	get 
+	{
+	return anyString;
+	}
+
+	set 
+	{
+	anyString = value;
+	}
+}
+
+public void helloPrint(string thisString)
+{
+
+Debug.Log("This is the requested: " + thisString);
+
+}
+
+}
---
+++ Inheritance Test/Assets/MyGameObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGameObject : HelloWorld {
+
+string betterString;
+
+HelloWorld helloWorld = new HelloWorld();
+
+void Start()
+{
+helloWorld.anyString = "This is wonderful!";
+Debug.Log("aaa " + anyString);
+//betterString = helloWorld.anyString;
+//helloWorld.helloPrint(betterString);
+}
+
+}
