1c3afdb2bf8d1d67269815b670ec2d2d3b03c790
Edward Ikeguchi <eikeguchi@mac.com>
2018-04-09 13:28:12 -0400

    wtf is this

--- Inheritance Test/Assets/MyGameObject.cs
+++ Inheritance Test/Assets/MyGameObject.cs
@@ -10,7 +10,7 @@ HelloWorld helloWorld = new HelloWorld();
 
 void Start()
 {
-helloWorld.anyString = "This is wonderful!";
+helloWorld.anyString = "This is not working!";
 Debug.Log("aaa " + anyString);
 //betterString = helloWorld.anyString;
 //helloWorld.helloPrint(betterString);
